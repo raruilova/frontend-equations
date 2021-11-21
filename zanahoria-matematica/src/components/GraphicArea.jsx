d8ae6c0ba7a9b531a797922c7c454c83bf381549
robinson <robinjosellove@gmail.com>
2021-11-21 13:15:17 -0500

    finished graphic

--- zanahoria-matematica/src/components/GraphicArea.jsx
+++ zanahoria-matematica/src/components/GraphicArea.jsx
@@ -1,43 +1,20 @@
-import React, { useContext} from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import "../styles/GraphicArea.scss";
 import logoZanahoria from "@logos/logo-zanahoria-matematica.png";
 import AppContext from "../context/AppContext";
-import {Chart} from 'react-google-charts';
-import DataRow from "./DataRow";
+import { Chart } from "react-google-charts";
 
 const GraphicArea = () => {
   const { state } = useContext(AppContext);
-  let results = [];
-  let tablex = [];
-  let resultsy = [];
-  let tabley = [];
-  
+
   const data = [
-    ['x', 'dogs'],
-    state.equation.payload.arraxy.map(result => [result.x, result.y]),
+    ["x", "y"],
+
   ];
-  state.equation.payload.arraxy.map(result => console.log([result.x, result.y]));
+  data.push(...state.equation.payload.arraxy.map((result) => [result.x, result.y]));
+
   
-
-  /*results.map((item) => console.log((item)));
-  
-
-  //console.log( state.equation.payload.arrayX);
-  results.forEach((item) => {
-    item.forEach((re) => {
-      tablex.push(re);
-    })
-  });
-  tablex.map(item => console.log(item));
-  //
-  results.forEach((item) => {
-    item.forEach((re) => {
-      tabley.push(re);
-    })
-  });
-  tabley.map(item => console.log(item));*/
-
   return (
     <div className="body-form">
       <div className="main">
@@ -52,21 +29,19 @@ const GraphicArea = () => {
                 src="https://ingenieriabasica.es/wp-content/uploads/2019/08/ecuacion-de-segundo-grado-1024x576.jpg"
                 alt=""
               />
-            <Chart
-    width={'600px'}
-    height={'400px'}
-    chartType="LineChart"
-    loader={<div>Loading Chart</div>}
-    data={data}
-    options={{
-      hAxis: {
-        title: 'Time',
-      },
-      vAxis: {
-        title: 'Popularity',
-      },
-    }}
-  />
+              <Chart
+                chartType="LineChart"
+                loader={<div>Loading Chart</div>}
+                data={data}
+                options={{
+                  hAxis: {
+                    title: "X",
+                  },
+                  vAxis: {
+                    title: "Y",
+                  },
+                }}
+              />
             </div>
           </section>
 
@@ -77,22 +52,19 @@ const GraphicArea = () => {
             </div>
             <div className="results-area-table">
               <h3>Tabla de valores</h3>
-              
-  <ul className="responsive-table">
-    <li className="table-header">
-      <div className="col col-1">X</div>
-      <div className="col col-2">Y</div>
-    </li>
-    {
-      state.equation.payload.arraxy.map((item, index) => (
-        <li className="table-row" key={index+1}>
-          <div className="col col-1" >{item.x}</div>
-          <div className="col col-2">{item.y}</div>
-         </li>
-      ))
-      }
-   
-  </ul>
+
+              <ul className="responsive-table">
+                <li className="table-header">
+                  <div className="col col-1">X</div>
+                  <div className="col col-2">Y</div>
+                </li>
+                {state.equation.payload.arraxy.map((item, index) => (
+                  <li className="table-row" key={index + 1}>
+                    <div className="col col-1">{item.x}</div>
+                    <div className="col col-2">{item.y}</div>
+                  </li>
+                ))}
+              </ul>
             </div>
           </section>
         </div>
